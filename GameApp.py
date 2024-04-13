9d84443c271a6511723178c1d4bf33e491e84b10
Caleb Christensen <calebwchristensen@gmail.com>
2024-04-13 16:25:09 -0600

    Add a 'best of' search function

--- GameApp.py
+++ GameApp.py
@@ -25,6 +25,7 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9 = st.tabs(['Overall', 'Abst
                                                                 'Party Games', "Strategy Games", 'War Games'])
 
 with st.sidebar:
+    st.header('Game Lookup')
     game = st.text_input('Enter a game name', value='Pandemic')
     if st.button('Random'):
         game = gameDf.sample().iloc[0,0]
@@ -143,3 +144,76 @@ st.subheader('Rating Distribution')
 ratingPlot = px.box(data_frame=yearDf, x = 'Rating')
 ratingPlot.update_xaxes(range=[5.5, 9])
 st.plotly_chart(ratingPlot)
+
+
+st.header('"Best of" search')
+topic = st.selectbox("Choose a metric:", ('Rating','Ranking', 'Complexity', 'Plays Last Month',
+                                          'Wants', 'Wishlists'))
+genre = st.selectbox("Choose a category:", ('Overall', 'Abstract', 'Customizable', 'Thematic',
+                                            'Family', 'Children', 'Party', 'Strategy', 'War'))
+
+if genre=='Overall':
+    if topic=='Ranking':
+        topDf=gameDf.sort_values('Overall Rank', ascending=True).head(5).dropna(axis=1, how='all')
+    else:
+        topDf=gameDf.sort_values(topic, ascending=False).head(5).dropna(axis=1, how='all')
+    st.dataframe(topDf)
+
+elif genre=='Abstract':
+    if topic=='Ranking':
+        topDf=absDf.sort_values('Abstract Rank', ascending=True).head(5).dropna(axis=1, how='all')
+    else:
+        topDf=absDf.sort_values(topic, ascending=False).head(5).dropna(axis=1, how='all')
+    st.dataframe(topDf)
+
+elif genre=='Customizable':
+    if topic=='Ranking':
+        topDf=custDf.sort_values('Customizable Rank', ascending=True).head(5).dropna(axis=1, how='all')
+    else:
+        topDf=custDf.sort_values(topic, ascending=False).head(5).dropna(axis=1, how='all')
+    st.dataframe(topDf)
+
+elif genre=='Thematic':
+    if topic=='Ranking':
+        topDf=themeDf.sort_values('Thematic Rank', ascending=True).head(5).dropna(axis=1, how='all')
+    else:
+        topDf=themeDf.sort_values(topic, ascending=False).head(5).dropna(axis=1, how='all')
+    st.dataframe(topDf)
+    
+elif genre=='Family':
+    if topic=='Ranking':
+        topDf=famDf.sort_values('Family Rank', ascending=True).head(5).dropna(axis=1, how='all')
+    else:
+        topDf=famDf.sort_values(topic, ascending=False).head(5).dropna(axis=1, how='all')
+    st.dataframe(topDf)
+    
+elif genre=='Children':
+    if topic=='Ranking':
+        topDf=childDf.sort_values('Children Rank', ascending=True).head(5).dropna(axis=1, how='all')
+    else:
+        topDf=childDf.sort_values(topic, ascending=False).head(5).dropna(axis=1, how='all')
+    st.dataframe(topDf)
+    
+elif genre=='Party':
+    if topic=='Ranking':
+        topDf=partyDf.sort_values('Party Rank', ascending=True).head(5).dropna(axis=1, how='all')
+    else:
+        topDf=partyDf.sort_values(topic, ascending=False).head(5).dropna(axis=1, how='all')
+    st.dataframe(topDf)
+    
+elif genre=='Strategy':
+    if topic=='Ranking':
+        topDf=stratDf.sort_values('Strategy Rank', ascending=True).head(5).dropna(axis=1, how='all')
+    else:
+        topDf=stratDf.sort_values(topic, ascending=False).head(5).dropna(axis=1, how='all')
+    st.dataframe(topDf)
+    
+elif genre=='War':
+    if topic=='Ranking':
+        topDf=warDf.sort_values('War Rank', ascending=True).head(5).dropna(axis=1, how='all')
+    else:
+        topDf=warDf.sort_values(topic, ascending=False).head(5).dropna(axis=1, how='all')
+    st.dataframe(topDf)
+    
+else:
+    st.markdown('Select a metric and a category')
